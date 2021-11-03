1e55e346dc78a629093a961c20b0358d59bdbbbe
giabbo79 <g.boccia@almaviva.it>
2021-11-03 10:23:59 +0100

    repo 2.4

--- Berksfile
+++ Berksfile
@@ -2,9 +2,9 @@ Encoding.default_external = "UTF-8"
 source 'https://supermarket.chef.io'
 metadata
 
-cookbook 'conda', github: "giabbo79/conda-chef", branch: "2.3"
-cookbook 'hops', github: "giabbo79/hops-hadoop-chef", branch: "2.3"
-cookbook 'ndb', github: "giabbo79/ndb-chef", branch: "2.3"
-cookbook 'kagent', github: "giabbo79/kagent-chef", branch: "2.3"
-cookbook 'tensorflow', github: "giabbo79/tensorflow-chef", branch: "2.3"
+cookbook 'conda', github: "giabbo79/conda-chef", branch: "2.4"
+cookbook 'hops', github: "giabbo79/hops-hadoop-chef", branch: "2.4"
+cookbook 'ndb', github: "giabbo79/ndb-chef", branch: "2.4"
+cookbook 'kagent', github: "giabbo79/kagent-chef", branch: "2.4"
+cookbook 'tensorflow', github: "giabbo79/tensorflow-chef", branch: "2.4"
 cookbook 'java', github: "giabbo79/java", branch: "v7.0.0-1"
--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,9 @@ include_attribute "tensorflow"
 include_attribute "hops_airflow"
 
 default['hopsmonitor']['user']                    = node['install']['user'].empty? ? "hopsmon" : node['install']['user']
+default['hopsmonitor']['user_id']                 = '1503'
 default['hopsmonitor']['group']                   = node['install']['user'].empty? ? "hopsmon" : node['install']['user']
+default['hopsmonitor']['group_id']                = '1503'
 
 default['hopsmonitor']['dir']                     = node['install']['dir'].empty? ? "/srv" : node['install']['dir']
 
@@ -27,6 +29,10 @@ default['prometheus']['version']                  = "2.10.0"
 default['prometheus']['url']                      = "#{node['download_url']}/prometheus/prometheus-#{node['prometheus']['version']}.linux-amd64.tar.gz"
 default['prometheus']['root_dir']                 = "#{node['hopsmonitor']['dir']}/prometheus"
 
+# Data volume directories
+default['prometheus']['data_volume']['root_dir']  = "#{node['data']['dir']}/prometheus"
+default['prometheus']['data_volume']['data_dir']  = "#{node['prometheus']['data_volume']['root_dir']}/prometheus-data"
+
 default['prometheus']['home']                     = "#{node['prometheus']['root_dir']}/prometheus-#{node['prometheus']['version']}.linux-amd64"
 default['prometheus']['base_dir']                 = "#{node['prometheus']['root_dir']}/prometheus"
 default['prometheus']['data_dir']                 = "#{node['prometheus']['root_dir']}/prometheus-data"
@@ -45,6 +51,10 @@ default['alertmanager']['port']                     = "9093"
 default['alertmanager']['version']                  = "0.17.0"
 default['alertmanager']['url']                      = "#{node['download_url']}/prometheus/alertmanager-#{node['alertmanager']['version']}.linux-amd64.tar.gz"
 default['alertmanager']['root_dir']                 = "#{node['hopsmonitor']['dir']}/alertmanager"
+
+# Data volume directories
+default['alertmanager']['data_volume']['root_dir']  = "#{node['data']['dir']}/alertmanager"
+default['alertmanager']['data_volume']['data_dir']  = "#{node['alertmanager']['data_volume']['root_dir']}/alertmanager-data"
 
 default['alertmanager']['home']                     = "#{node['alertmanager']['root_dir']}/alertmanager-#{node['alertmanager']['version']}.linux-amd64"
 default['alertmanager']['base_dir']                 = "#{node['alertmanager']['root_dir']}/alertmanager"
